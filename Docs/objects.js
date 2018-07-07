79ce4dad4be889409b69750b49b5631480a0014f
Roy Prigat <rp2719@columbia.edu>
2018-07-08 00:07:35 +0300

    committing changes Sun Jul  8 00:07:35 IDT 2018

--- Docs/objects.js
+++ Docs/objects.js
@@ -1,3 +1,26 @@
+// an object constructor using a function
+function Car(model, year) {
+    this.model = model;
+    this.year = year;
+};
+
+const Animal = {
+    type: 'Invertebrates', // Default value of properties
+    displayType: function () { // Method which will display type of Animal
+        return this.type;
+    }
+};
+
+// Initialize a car instance
+let mycar = new Car('Talon TSi', 1993);
+console.log(Object.getOwnPropertyNames(mycar));
+
+// Object.create() method
+
+const dog = Object.create(Animal);
+console.log(dog.displayType());
+
+
 // primitives are auto converted to Objects(wrappers)
 
 
@@ -9,4 +32,3 @@
 
 // object constant
 
-//
