775539c4608c4803134cbe8293ce677d56b69d74
Roy Prigat <rp2719@columbia.edu>
2018-07-12 19:08:52 +0300

    linkedlist

--- Data_Structures/linked_list.js
+++ Data_Structures/linked_list.js
@@ -27,20 +27,61 @@ class LinkedList {
         this.tail.next = newNode;
         this.tail = newNode;
     }
+    delete(value) {
+        // Empty list, return null
+        if (!this.head) {throw 'Cannot delete from empty list...';}
+
+        // If head is to be deleted, make head the 2nd node
+        if(this.head.data == value) {
+            this.head = this.head.next;
+            return
+        }
+
+        // Iterate list, find value and remove it
+        let current = this.head;
+        while (current.next) {
+            if (current.next.data == value) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+    }
+    peek() {
+        console.log(this.tail.data);
+    }
     toString() {
-        let index = this.head;
-        if(index) {console.log(index.data)}
-        while(index.next) {
-            console.log(index.next.data);
-            index.next = index.next.next;
+        if(!this.head) {throw 'List is empty...';}
+        this.tail = this.head;
+        while(this.tail) {
+            console.log(this.tail.data);
+            if (this.tail.next) {this.tail = this.tail.next;}
+            else {return;}
         }
     }
 }
 
+
+
+// testing
 let list = new LinkedList();
 
+// append, prepend
+console.log("Prepend-Append:");
 list.append(4);
-list.append(5);
-list.append(6);
 list.prepend(1);
+list.append(6);
+list.prepend(10);
 list.toString();
+
+// delete
+console.log("Delete:");
+list.delete(10);
+list.delete(5);
+list.delete(4);
+list.delete(1);
+list.toString();
+
+// peek
+console.log("Peek");
+list.peek();
